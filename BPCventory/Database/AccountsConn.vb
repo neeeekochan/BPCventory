4a90db243ea9f42ede347d72363649ddada50a10
Nicko Osabel <nickoosabel@gmail.com>
2022-12-11 17:07:54 +0800

    My work

---
+++ BPCventory/Database/AccountsConn.vb
@@ -0,0 +1,22 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class accountsConn
+    Public connToAcc As New MySqlConnection("server=localhost;username=root;password=;database=BPC_accounts")
+    Public Function openAccDB()
+        Try
+            connToAcc.Open()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return connToAcc
+    End Function
+
+    Public Function closeAccDB()
+        Try
+            connToAcc.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return connToAcc
+    End Function
+End Class
